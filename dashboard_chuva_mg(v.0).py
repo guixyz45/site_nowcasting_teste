4ea52896be91ec1090185f333d5c92c6b189ce37
guixyz45 <87347272+guixyz45@users.noreply.github.com>
2024-12-04 18:20:00 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -2,21 +2,38 @@ import streamlit as st
 import pandas as pd
 import geopandas as gpd
 import requests
-import io
 import numpy as np
 from datetime import datetime, timedelta
 import leafmap.foliumap as leafmap
 import folium
+import glob
+import calendar
+from io import StringIO
+import matplotlib.pyplot as plt
 from folium.plugins import MarkerCluster
-import plotly.express as px
 
 # URLs e caminhos de arquivos
-mg_shp_url = 'https://github.com/giuliano-macedo/geodata-br-states/raw/main/geojson/br_states/br_mg.json'
-csv_file_path = 'input;/lista_das_estacoes_CEMADEN_13maio2024.csv'
+shp_mg_url = 'https://github.com/giuliano-macedo/geodata-br-states/raw/main/geojson/br_states/br_mg.json'
+csv_file_path = 'input;/filtered_data.csv'
 
 # Login e senha do CEMADEN (previamente fornecidos)
-login = 'd2020028915@unifei.edu.br'
-senha = 'gLs24@ImgBr!'
+login = 'augustoflaviobob@gmail.com'
+senha = 'Flaviobr123!'
+
+# Carregar os dados do shapefile de Minas Gerais
+mg_gdf = gpd.read_file(shp_mg_url)
+
+# Estações Selecionadas do Sul de Minas Gerais
+# codigo_estacao = ['314790701A','310710901A','312870901A','315180001A','316930702A','314780801A','315250101A','313240401A','313360001A','311410501A','311360201A','313300601A']
+codigo_estacao = ['315180001A','316930702A']
+
+
+# Carregar os dados das estações
+df1 = pd.read_csv(csv_file_path)
+gdf = gpd.GeoDataFrame(df1, geometry=gpd.points_from_xy(df1['longitude'], df1['latitude']))
+
+# Realizar o filtro espacial: apenas estações dentro de Minas Gerais
+gdf_mg = gpd.sjoin(gdf, mg_gdf, predicate='within')
 
 # Recuperação do token
 token_url = 'http://sgaa.cemaden.gov.br/SGAA/rest/controle-token/tokens'
@@ -25,144 +42,185 @@ response = requests.post(token_url, json=login_payload)
 content = response.json()
 token = content['token']
 
-# Carregar os dados do shapefile de Minas Gerais
-mg_gdf = gpd.read_file(mg_shp_url)
+# sigla do estado do Brasil
+sigla_estado = 'MG'
 
-# Carregar os dados das estações
-df = pd.read_csv(csv_file_path)
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['Longitude'], df['Latitude']))
+# Data de hoje
+agora = datetime.now()
 
-# Filtrar apenas as estações dentro de Minas Gerais
-gdf_mg = gpd.sjoin(gdf, mg_gdf, predicate='within')
+# Dia, mês e ano de hoje
+dia_atual = agora.day
+mes_atual = agora.month
+ano_atual = agora.year
 
-# Lista das estações desejadas
-codigo_estacao = ['314790701A', '310710901A', '312870901A', '315180001A', '316930701A',
-                  '314780801A', '315250101A', '313240401A', '313360001A', '311410501A',
-                  '316230201A', '313300601A']
+# Calcula o mês e ano anteriores para a data inicial
+if mes_atual == 1:
+    mes_anterior = 12
+    ano_anterior = ano_atual - 1
+else:
+    mes_anterior = mes_atual - 1
+    ano_anterior = ano_atual
+    mes_pos = mes_atual + 1
 
-# Filtrar as estações desejadas no GeoDataFrame
-gdf_mg = gdf_mg[gdf_mg['Código'].isin(codigo_estacao)]
+# Formata as datas
+diai = '01'
+data_inicial = f'{ano_atual}{mes_anterior:02d}{diai}'
+data_final = f'{ano_atual}{mes_atual:02d}{dia_atual:02d}'
+data_inicial = pd.to_datetime(data_inicial)
+data_final = pd.to_datetime(data_final)
 
-# Função de solicitação de dados de precipitação
-def request_data(inicio, fim, uf, municipio, sws_url="http://sws.cemaden.gov.br/PED/rest/pcds/dados_rede"):
-    date = datetime.strptime(inicio, "%Y%m%d%H%M")
-    fim = datetime.strptime(fim, "%Y%m%d%H%M")
-    combined_df = None
+def baixar_dados_estacoes(codigo_estacao, data_inicial, data_final, sigla_estado):
+    # Lista para armazenar os dados de todas as estações
+    dados_estacoes = {}
 
-    while date <= fim:
-        print(f"Requesting data from: {date}")
-        start_date = date.strftime("%Y%m%d%H%M")
-        end_date = (date + timedelta(days=1)).strftime("%Y%m%d%H%M")
-        params = dict(sensor=10, uf=uf, municipio=municipio, inicio=start_date, fim=end_date, formato="json")
+    for codigo in codigo_estacao:
+        # Lista para armazenar os dados de cada mês de uma estação
+        dados_completos = []
 
-        r = requests.get(sws_url, params=params, headers={'token': token})
-        data = io.StringIO(r.text)
+        for ano_mes_dia in pd.date_range(data_inicial, data_final, freq='M'):
+            ano_mes = ano_mes_dia.strftime('%Y%m')  # Formato '202401'
 
-        try:
-            df = pd.read_csv(data, sep=';', skiprows=1, encoding="ISO-8859-1")
-            df = df.drop(columns=['sensor', 'qualificacao', 'offset'])
+            # URL e parâmetros da requisição
+            sws_url = 'http://sws.cemaden.gov.br/PED/rest/pcds/dados_pcd'
+            params = dict(
+                rede=11, uf=sigla_estado, inicio=ano_mes, fim=ano_mes, codigo=codigo
+            )
+            
+            # Requisição dos dados
+            r = requests.get(sws_url, params=params, headers={'token': token})
+            dados = r.text
+
+            # Remover a linha de comentário e converter para DataFrame
+            linhas = dados.split("\n")
+            dados_filtrados = "\n".join(linhas[1:])  # Remove a primeira linha (comentário)
+            
+            df = pd.read_csv(StringIO(dados_filtrados), sep=";")
+
+            # Filtra somente os dados de chuva
+            df = df[df['sensor'] == 'chuva']
+
+            # Converte e organiza os dados
             df['datahora'] = pd.to_datetime(df['datahora'])
-        except Exception as e:
-            print(f"Error occurred: {e}")
-            continue
+            df.set_index('datahora', inplace=True)
 
-        pivot_df = df.pivot(index='datahora', columns='cod.estacao', values='valor')
-        stations_info = df[['cod.estacao', 'nome', 'municipio', 'latitude', 'longitude']].drop_duplicates().set_index('cod.estacao')
+            # Armazena os dados no acumulado
+            dados_completos.append(df)
 
-        if combined_df is None:
-            combined_df = pd.concat([stations_info.T, pivot_df], axis=0)
-        else:
-            pivot_df = pivot_df.reindex(columns=combined_df.columns, fill_value=np.nan)
-            combined_df = pd.concat([combined_df, pivot_df], axis=0)
+        # Combina os dados de todos os meses para a estação
+        if dados_completos:
+            dados_estacoes[codigo] = pd.concat(dados_completos)
 
-        date += timedelta(days=1)
+    return dados_estacoes
 
-    return combined_df
+# Função para exibir gráficos de precipitação
+def mostrar_graficos():
+    horas = ['Última Hora', '24 Horas', '48 Horas']
+    chuva_valores = [dfuma, soma_ultimas_24h, soma_ultimas_48h]
 
+    fig, ax = plt.subplots(figsize=(3, 2))
+    ax.bar(horas, chuva_valores, color=['blue', 'orange', 'green'])
+    ax.set_ylabel('Precipitação (mm)')
+    ax.set_title('Precipitação nas últimas horas')
 
-# Função principal do dashboard
-def main():
-    hoje = datetime.now()
-    data_inicial = hoje.replace(day=1)
-    data_final = hoje
+    st.pyplot(fig)
+# Função para exibir o pop-up no canto inferior direito
+def exibir_popup(chuva_ultima_hora, chuva_ultimas_24_horas, chuva_ultimas_48_horas):
+    st.markdown("""
+    <style>
+        .popup {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            width: 250px;
+            background-color: rgba(255, 255, 255, 0.8);
+            color: black;
+            padding: 10px;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            font-family: Arial, sans-serif;
+        }
+    </style>
+    """, unsafe_allow_html=True)
 
-    st.set_page_config(layout="wide")
-
-    st.markdown(
-        """
-        <style>
-            .main .block-container {
-                padding: 0;
-                margin: 0;
-            }
-            iframe {
-                height: 100vh !important;
-                width: 100vw !important;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-    # Mapa interativo usando Leafmap
-    m = leafmap.Map(center=[-18.5122, -44.5550], zoom=7, draw_control=False, measure_control=False, fullscreen_control=False, attribution_control=True)
+    # Conteúdo do popup
+    st.markdown(f"""
+    <div class="popup">
+        <h4>Informações de Chuva</h4>
+        <p>Chuva na última hora: {dfuma} mm</p>
+        <p>Chuva nas últimas 24 horas: {soma_ultimas_24h} mm</p>
+        <p>Chuva nas últimas 48 horas: {soma_ultimas_48h} mm</p>
+    </div>
+    """, unsafe_allow_html=True)
     
-    # Criar um cluster de marcadores para agrupar os marcadores no mapa
-    marker_cluster = MarkerCluster().add_to(m)
+m = leafmap.Map(center=[-21, -45],zoom_start = 8,draw_control=False, measure_control=False, fullscreen_control=False, attribution_control=True)
 
-    # Adicionar marcadores das estações meteorológicas selecionadas
-    for i, row in gdf_mg.iterrows():
-        folium.CircleMarker(
-            location=[row['Latitude'], row['Longitude']],
-            radius=8,
-            color='blue',
-            fill=True,
-            fill_color='white',
-            fill_opacity=0.6,
-            popup=f"{row['Nome']} (Código: {row['Código']})"
-        ).add_to(marker_cluster)
+# Defina o layout da página como largo
+st.set_page_config(layout="wide")
 
-    # Sidebar para seleção de estação e datas
-    st.sidebar.header("Filtros de Seleção")
+# Adicionar marcadores das estações meteorológicas
+for i, row in gdf_mg.iterrows():
+    # Baixar dados da estação
+    dados = baixar_dados_estacoes(codigo_estacao, data_inicial, data_final, sigla_estado)
 
-    # Adicionar dropdown com a lista de estações filtradas
-    estacao_selecionada = st.sidebar.selectbox("Selecione a Estação", gdf_mg['Nome'].unique())
-    codigo_estacao_selecionada = gdf_mg[gdf_mg['Nome'] == estacao_selecionada]['Código'].values[0]
+    # Adicionar marcador com valor
+    folium.RegularPolygonMarker(
+        location=[row['latitude'], row['longitude']],
+        color='black',
+        opacity=1,
+        weight=1,
+        fillColor='green',
+        fillOpacity=1,
+        numberOfSides=4,
+        rotation=45,
+        radius=8,
+        popup=f"{row['municipio']} (Código: {row['codEstacao']})"
+    ).add_to(m)
 
-    sigla_estado = 'MG'
+m.add_gdf(
+    mg_gdf, 
+    layer_name="Minas Gerais", 
+    style={"color": "black", "weight": 1, "fillOpacity": 0, "interactive": False},
+    info_mode=None
+)
 
-    tipo_busca = st.sidebar.radio("Tipo de Busca:", ('Diária', 'Mensal'))
+st.sidebar.header("Filtros de Seleção")
+modo_selecao = st.sidebar.radio("Selecionar Estação por:", ('Código'))
 
-    if tipo_busca == 'Diária':
-        data_inicial = st.sidebar.date_input("Data Inicial", value=data_inicial)
-        data_final = st.sidebar.date_input("Data Final", value=data_final)
+if modo_selecao == 'Código':
+    estacao_selecionada = st.sidebar.selectbox("Selecione a Estação", gdf_mg['codEstacao'].unique())
+    codigo_estacao = gdf_mg[gdf_mg['codEstacao'] == estacao_selecionada]['codEstacao'].values[0]
+
+sigla_estado = 'MG'
+tipo_busca = st.sidebar.radio("Tipo de Busca:", ('Diária'))
+
+if tipo_busca == 'Diária':
+    data_inicial = st.sidebar.date_input("Data", value=data_inicial)
+else:
+    ano_selecionado = st.sidebar.selectbox("Selecione o Ano", range(2020, datetime.now().year + 1))
+    mes_selecionado = st.sidebar.selectbox("Selecione o Mês", range(1, 13))
+    data_inicial = datetime(ano_selecionado, mes_selecionado, 1)
+    data_final = datetime(ano_selecionado, mes_selecionado + 1, 1) - timedelta(days=1) if mes_selecionado != 12 else datetime(ano_selecionado, 12, 31)
+
+if st.sidebar.button("Baixar Dados"):
+    data_inicial_str = data_inicial.strftime('%Y%m%d')
+    data_final_str = data_final.strftime('%Y%m%d')
+    dados_baixados = dados['codEstacao']
+
+    if not dados_estacao.empty:
+        st.subheader(f"Dados da Estação: {estacao_selecionada} (Código: {codigo_estacao})")
+        st.write(dados_baixados)
     else:
-        ano_selecionado = st.sidebar.selectbox("Selecione o Ano", range(2020, datetime.now().year + 1))
-        mes_selecionado = st.sidebar.selectbox("Selecione o Mês", range(1, 13))
-        data_inicial = datetime(ano_selecionado, mes_selecionado, 1)
-        data_final = (datetime(ano_selecionado, mes_selecionado + 1, 1) - timedelta(days=1)) if mes_selecionado != 12 else datetime(ano_selecionado, 12, 31)
+        st.warning("Nenhum dado encontrado para o período selecionado.")
 
-    if st.sidebar.button("Mostrar Gráfico"):
-        data_inicial_str = data_inicial.strftime('%Y%m%d%H%M')
-        data_final_str = data_final.strftime('%Y%m%d%H%M')
-        dados_estacao = request_data(data_inicial_str, data_final_str, sigla_estado, estacao_selecionada)
+# Checkbox na barra lateral para alternar exibição do gráfico
+mostrar = st.sidebar.checkbox("Gráfico de Precipitação")
 
-        if not dados_estacao.empty:
-            st.subheader(f"Gráfico de Precipitação - Estação: {estacao_selecionada} (Código: {codigo_estacao_selecionada})")
+# Exibir ou ocultar o gráfico conforme o estado do checkbox
+if mostrar:
+    mostrar_graficos()
+st.dataframe(dados)
 
-            dados_estacao['datahora'] = pd.to_datetime(dados_estacao['datahora'])
-            if tipo_busca == 'Diária':
-                dados_diarios = dados_estacao.resample('D', on='datahora').sum()
-                fig = px.line(dados_diarios, x=dados_diarios.index, y='valor', title='Precipitação Diária', labels={'valor': 'Precipitação (mm)'})
-            else:
-                dados_mensais = dados_estacao.resample('M', on='datahora').sum()
-                fig = px.bar(dados_mensais, x=dados_mensais.index, y='valor', title='Precipitação Mensal', labels={'valor': 'Precipitação (mm)'})
-
-            st.plotly_chart(fig)
-        else:
-            st.warning("Nenhum dado encontrado para o período selecionado.")
-
-    m.to_streamlit()
-
-if __name__ == "__main__":
-    main()
+# Mostrar o mapa em Streamlit
+m.to_streamlit(width=1300,height=775)
+# Chamando a função para exibir o popup
+exibir_popup(chuva_ultima_hora, chuva_ultimas_24_horas, chuva_ultimas_48_horas)
